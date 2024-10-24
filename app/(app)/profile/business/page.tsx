9a9687f9985dcb92653835af5c29a1f4d2327cfc
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-10-24 13:26:06 +0700

    show polygon in profile (and marker)

--- app/(app)/profile/business/page.tsx
+++ app/(app)/profile/business/page.tsx
@@ -2,10 +2,16 @@
 
 import Map3D from "@/components/Map3D";
 import { Button } from "@/components/ui/button";
+import {
+  BUSINESS_REGIS_POLYGON,
+  BUSINESS_REGISTRATION_MARKER,
+  polygonOptions,
+} from "@/const/maps";
 import { useUser } from "@/contexts/user/UserContext";
 import privateRoute from "@/hooks/privateRoute";
 import { config, storage } from "@/lib/appwrite";
 import { loader } from "@/lib/maps";
+import { removeElementsWithClass } from "@/utils/maps";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useRef, useState } from "react";
@@ -31,17 +37,67 @@ function BusinessProfilePage() {
 
   // once user's business loads, fly camera
   useEffect(() => {
-    if (currentUser?.business && map) {
-      // @ts-ignore
-      map.flyCameraTo({
-        endCamera: {
-          center: { lat: 40.6191, lng: -122.3816, altitude: 100 },
-          tilt: 67.5,
-          range: 1000,
-        },
-        durationMillis: 5000,
-      });
-    }
+    loader.load().then(async () => {
+      if (currentUser?.business && map) {
+        // @ts-ignore
+        const { Marker3DElement } = (await google.maps.importLibrary(
+          "maps3d"
+        )) as google.maps.Maps3DLibrary;
+        // marker and fly camera
+        if (
+          currentUser.business.positionLat &&
+          currentUser.business.positionLng
+        ) {
+          // @ts-ignore
+          map.flyCameraTo({
+            endCamera: {
+              center: {
+                lat: currentUser.business.positionLat,
+                lng: currentUser.business.positionLng,
+                altitude: 100,
+              },
+              tilt: 67.5,
+              range: 1000,
+            },
+            durationMillis: 5000,
+          });
+
+          removeElementsWithClass(BUSINESS_REGISTRATION_MARKER);
+
+          const marker = new Marker3DElement({
+            position: {
+              lat: currentUser.business.positionLat,
+              lng: currentUser.business.positionLng,
+            },
+            label: currentUser.business.name,
+          });
+
+          marker.classList.add(BUSINESS_REGISTRATION_MARKER);
+          map.append(marker);
+        }
+
+        // polygon
+        if (currentUser.business.polygonPositions.length > 2) {
+          try {
+            const positions = currentUser.business.polygonPositions.map((p) =>
+              JSON.parse(p)
+            );
+
+            removeElementsWithClass(BUSINESS_REGIS_POLYGON);
+
+            const polygon = new google.maps.maps3d.Polygon3DElement(
+              polygonOptions
+            );
+            polygon.outerCoordinates = positions.map((c) => ({
+              ...c,
+              altitude: 300,
+            }));
+            polygon.classList.add(BUSINESS_REGIS_POLYGON);
+            map.append(polygon);
+          } catch (error) {}
+        }
+      }
+    });
   }, [currentUser?.business, map]);
 
   const router = useRouter();
