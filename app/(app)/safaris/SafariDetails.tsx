750679c3eb0ebd8a6331db242ce8bc2505c59346
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-11-05 09:17:05 +0700

    polygons details

--- app/(app)/safaris/SafariDetails.tsx
+++ app/(app)/safaris/SafariDetails.tsx
@@ -8,8 +8,8 @@ import { Button } from "@/components/ui/button";
 import { GroundPoint } from "@/components/Point";
 import { Eye, Trash, X } from "lucide-react";
 import { CollapsibleContext } from "@/components/CollapsibleController";
-import { FlyCameraOptions } from "@/type/maps";
-import { getElevation, getElevationforPoint } from "@/lib/maps";
+import { FlyCameraOptions, LatLng } from "@/type/maps";
+import { findCenter, getElevation, getElevationforPoint } from "@/lib/maps";
 import { config, databases } from "@/lib/appwrite";
 import { useToast } from "@/hooks/use-toast";
 import Image from "next/image";
@@ -157,7 +157,7 @@ export default function SafariDetails({
               ))}
             </div>
           ) : (
-            <div className="flex items-center justify-center flex-col gap-4">
+            <div className="flex items-center justify-center flex-col gap-4 py-4">
               <Image
                 src="/spot-marker.svg"
                 width={200}
@@ -181,8 +181,122 @@ export default function SafariDetails({
         <TabsContent value="polygons" className="space-y-4 grow ">
           <div className="text-[0.8rem] text-muted-foreground text-center">
             Polygons for places, buildings, etc. that you&apos;ve highlighted.
+            Switch to{" "}
+            <span
+              className="text-primary hover:text-primary/70 cursor-pointer"
+              onClick={() => {
+                setSafariViewMode(SafariViewMode.POLYGON);
+                toast({
+                  description: "Click anywhere to map your Polygon points",
+                });
+              }}
+            >
+              "Polygon Mode"
+            </span>{" "}
+            to create Polygons.
           </div>
-          <div>Current Polygons: {polygons.length}</div>
+          {polygons.length > 0 ? (
+            <div className="flex flex-col gap-2">
+              {polygons.map((pol) => {
+                const center = findCenter(
+                  pol.points.map((p) => JSON.parse(p) as LatLng)
+                );
+
+                return (
+                  <div
+                    key={pol.$id}
+                    className="p-4 text-sm border border-border rounded-md space-y-2"
+                  >
+                    <div className="gap-2 justify-between flex items-center">
+                      <div>{pol.title}</div>
+                      <div className="flex gap-2 items-center">
+                        <Button
+                          disabled={isDeleting}
+                          variant="outline"
+                          size="icon"
+                          onClick={async () => {
+                            if (map) {
+                              const opts: FlyCameraOptions = {
+                                endCamera: {
+                                  center: {
+                                    lat: center.latitude,
+                                    lng: center.longitude,
+                                    altitude: await getElevation({
+                                      latitude: center.latitude,
+                                      longitude: center.longitude,
+                                    }),
+                                  },
+                                  range: 1000,
+                                  tilt: 67.5,
+                                },
+                                durationMillis: 1000,
+                              };
+                              // @ts-ignore
+                              map.flyCameraTo(opts);
+                            }
+                          }}
+                        >
+                          <Eye />
+                        </Button>
+                        <Button
+                          onClick={async () => {
+                            try {
+                              setisDeleting(true);
+                              await databases.deleteDocument(
+                                config.dbId,
+                                config.polygonCollectionId,
+                                pol.$id
+                              );
+                              setSafariPolygons((prev) =>
+                                prev.filter((curPols) => curPols.$id != pol.$id)
+                              );
+                            } catch (error) {
+                              toast({
+                                variant: "destructive",
+                                title: "Error deleting polygon",
+                                description:
+                                  error instanceof Error
+                                    ? error.message
+                                    : "Something went wrong.",
+                              });
+                            } finally {
+                              setisDeleting(false);
+                            }
+                          }}
+                          disabled={isDeleting}
+                          variant="destructive"
+                          size="icon"
+                        >
+                          <Trash />
+                        </Button>
+                      </div>
+                    </div>
+                    <GroundPoint point={center} />
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div className="flex items-center justify-center flex-col gap-4 py-4">
+              <Image
+                src="/polygon-marker.svg"
+                width={200}
+                height={200}
+                alt="Empty Spots"
+                className="w-56"
+              />
+              <Button
+                onClick={() => {
+                  setSafariViewMode(SafariViewMode.POLYGON);
+                  toast({
+                    description: "Click anywhere to map your Polygon points",
+                  });
+                }}
+              >
+                Create Polygons
+              </Button>
+            </div>
+          )}{" "}
         </TabsContent>
       </Tabs>
     </div>
