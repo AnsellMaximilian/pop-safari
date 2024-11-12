8a50048e949c9d33afb2984eeaacd11714c156d1
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-11-12 09:12:26 +0700

    remove old structure

--- app/(app)/safaris/FoundPlace.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { PlaceData } from "@/type/maps";
-import React from "react";
-
-export default function FoundPlace({ place }: { place: PlaceData }) {
-  return <div>FoundPlace</div>;
-}
