e0fe140b2c376f5023c678a18860db914756f755
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-11-17 09:05:15 +0700

    scroll fix

--- app/app/safaris/SafariView.tsx
+++ app/app/safaris/SafariView.tsx
@@ -212,7 +212,7 @@ export default function SafariView({ safari }: { safari: Safari }) {
           className="absolute right-4 top-44 bottom-4 z-10"
           OpenIcon={MapPinHouse}
           contents={(setOpen) => (
-            <div className="rounded-md shadow-md bg-white p-4 w-[500px] grow">
+            <div className="rounded-md shadow-md bg-white p-4 w-[500px] grow overflow-y-auto">
               <div className="flex justify-between items-center mb-4">
                 <h2 className="font-semibold">Found Place</h2>
                 <Button
